e1399018606af5a049f849ff7cd1a07a37612f6b
JustChillinBro92 <ayanpurkait11@gmail.com>
2025-08-27 02:09:48 +0530

    data transfer to backend added

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
 import Navbar from './components/Navbar/navabar'
 import Sidebar from './components/Sidebar/Sidebar'
+import Add from './pages/Add/Add'
+import List from './pages/List/List'
+import Orders from './pages/Orders/Orders'
+
 import './index.css'
+
 
 const App = () => {
   return (
@@ -10,6 +17,11 @@ const App = () => {
       <hr />
       <div className="app-content">
         <Sidebar/>
+        <Routes>
+          <Route path="/add" element={<Add/>}/>
+          <Route path="/list" element={<List/>}/>
+          <Route path="/orders" element={<Orders/>}/>
+        </Routes>
       </div>
     </div>
   )
--- admin/src/components/Sidebar/Sidebar.jsx
+++ admin/src/components/Sidebar/Sidebar.jsx
@@ -1,28 +1,30 @@
 import React from "react";
-import "./Sidebar.css";
-import { assets } from "../../assets/assets";
+import { NavLink } from "react-router-dom";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBowlFood } from '@fortawesome/free-solid-svg-icons'
 import { faList } from "@fortawesome/free-solid-svg-icons";
 import { faCirclePlus } from "@fortawesome/free-solid-svg-icons";
 
+import "./Sidebar.css";
+
+
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebar-contents">
-        <div className="sidebar-option">
+        <NavLink to="/add" className="sidebar-option">
         <FontAwesomeIcon icon={faCirclePlus} />
           <p>Add Items</p>
-        </div>
-        <div className="sidebar-option">
+        </NavLink>
+        <NavLink to="/list" className="sidebar-option">
           <FontAwesomeIcon icon={faList} />
           <p>List Items</p>
-        </div>
-        <div className="sidebar-option">
+        </NavLink>
+        <NavLink to="/orders" className="sidebar-option">
           <FontAwesomeIcon icon={faBowlFood} />
           <p>Orders</p>
-        </div>
+        </NavLink>
       </div>
     </div>
   );
