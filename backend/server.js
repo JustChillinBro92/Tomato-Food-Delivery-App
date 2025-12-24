dd0a61d69631dbfd7b1536a34b3f727164968fb5
JustChillinBro92 <ayanpurkait11@gmail.com>
2025-12-24 17:29:24 +0530

    render.yaml added

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import restaurantRouter from "./routes/restaurantRouter.js"
 
 // app config
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 // middleware
 app.use(express.json());
